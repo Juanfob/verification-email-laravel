24fb989a76281ec0aff11b89f3f0f7f8be4b0dcd
Juan Fermin <juanfob.dev@gmail.com>
2019-12-08 14:11:19 +0100

    First upload

---
+++ config/mail.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    "driver" => "smtp",
+    "host" => "smtp.mailtrap.io",
+    "port" => 2525,
+    "from" => array(
+        "address" => "no-reply@upswingers.com",
+        "name" => "UpSwingers"
+    ),
+    "username" => "e8565d2c3dbe2e",
+    "password" => "0b2e3c5b0eed6b",
+    "sendmail" => "/usr/sbin/sendmail -bs"
+];
---
+++ resources/lang/es/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'Verify Your Email Address' => 'Verifica tu email desde es/auth/ .',
+    'click here to request another' => 'Click aqui para solicitar otro desde es/auth/ .',
+    'Verify Email Address' => 'Verifica tu email en español desde es/auth/ .'
+
+];
